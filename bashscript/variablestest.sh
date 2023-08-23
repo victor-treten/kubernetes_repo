4b9dcd30722e95f5710317a43093a11e6e8f040e
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-08-23 11:18:38 +0100

    variable example file

---
+++ bashscript/variablestest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+intA=20
+floatB=20.20
+stringA="first_string"
+DIR_PATH="/tmp"
+
+echo "############################"
+echo "Value of inter A is $intA"
+echo
+echo
+
+echo "############################"
+echo "Value of Float B is $floatB"
+echo
+echo
+
+echo "############################"
+echo "Value of string A is $stringA"
+echo
+echo
+
+echo "############################"
+echo "Directory path is $DIR_PATH"
+echo 
+echo
+
+ls $DIR_PATH
