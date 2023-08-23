4656e431ba13f9c3855eff5d34e0efc84a28df0e
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-08-23 10:20:48 +0100

    automat website with script

---
+++ bashscript/wesetup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+## This is a script to host a website with html ##
+
+# Installing Dependencies
+echo "###########################################"
+echo "Installing packages"
+echo "###########################################"
+sudo yum install wget unzip httpd -y
+echo
+
+
+# Start & Enable Service
+echo "###########################################"
+echo "start and enable httpd"
+echo "###########################################"
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo
+
+# Creating Temp Directory
+echo "###########################################"
+echo "Starting artifact deployment"
+echo "###########################################"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+echo
+
+wget https://www.tooplate.com/zip-templates/2135_mini_finance.zip
+unzip 2135_mini_finance.zip
+sudo cp -r 2135_mini_finance/* /var/www/html
+echo
+
+# Bounce Service
+echo "###########################################"
+echo "restarting httpd service"
+echo "###########################################"
+systemclt restart httpd
+echo
+
+#clean up
+echo "###########################################"
+echo "removing temporary files"
+echo "###########################################"
+rm -fr /tmp/webfiles
+echo
+
+
+echo "###########################################"
+echo "list the content of /var/www/html"
+echo "###########################################"
+
+echo "###########################################"
+echo "Restart httpd again"
+echo "###########################################"
+systemctl restart httpd
+
+echo "###########################################"
+echo "Print out the ip address"
+echo "###########################################"
+ip addr show
