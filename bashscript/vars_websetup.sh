9c0bd611ac9369c60a2ae6b304bba9802be3eaab
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-08-23 11:29:37 +0100

    variable websetup

---
+++ bashscript/vars_websetup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Variable Delcaration
+PACKAGES="httpd wget unzip"
+SVC=httpd
+URL="https://www.tooplate.com/zip-templates/2135_mini_finance.zip"
+ART_NAME="2135_mini_finance"
+TEMPDIR="/tmp/webfiles"
+
+
+
+
+
+## This is a script to host a website with html ##
+
+# Installing Dependencies
+echo "###########################################"
+echo "Installing packages"
+echo "###########################################"
+sudo yum install $PACKAGES -y > /dev/null
+echo
+
+
+# Start & Enable Service
+echo "###########################################"
+echo "start and enable httpd"
+echo "###########################################"
+sudo systemctl start $SVC
+sudo systemctl enable $SVC
+echo
+
+# Creating Temp Directory
+echo "###########################################"
+echo "Starting artifact deployment"
+echo "###########################################"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+echo
+
+wget $URL > /dev/null
+unzip $ART_NAME.zip > /dev/null
+sudo cp -r $ART_NAME/* /var/www/html
+echo
+
+# Bounce Service
+echo "###########################################"
+echo "restarting httpd service"
+echo "###########################################"
+sudo systemclt restart $SVC
+echo
+
+#clean up
+echo "###########################################"
+echo "removing temporary files"
+echo "###########################################"
+sudo rm -fr $TEMPDIR
+echo
+
+
+echo "###########################################"
+echo "list the content of /var/www/html"
+echo "###########################################"
+ls /var/www/html
+
+echo "###########################################"
+echo "Restart httpd again"
+echo "###########################################"
+sudo systemctl restart $SVC
+
+echo "###########################################"
+echo "Print out the ip address"
+echo "###########################################"
+ip addr show
